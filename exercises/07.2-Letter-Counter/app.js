c9b781a5d1ad0f199b325103a6ce57a943d65987
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-11 02:33:19 +0000

    Ejercicios de array por la noche

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,11 +1,11 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
 
-for(let i=0; i<par.length; i++){
+
+for(let i=0; i < par.length ; i++){
     let letter = par[i].toLowerCase();
-    if(letter === " ") continue
-    else if(counts[letter] === undefined) counts[letter] = 1
-    else (counts[letter] = counts[letter]+1)
-   
+    if (letter === " ") continue;
+    else if(counts[letter] === undefined) counts[letter] = 1;
+    else(counts[letter] = counts[letter]+1);
 }
 console.log(counts);
--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,10 @@
 var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
-//your code here
+let dataType = () => {
+    var types = [];
+    for(let i = 0; i < mix.length; i++ ){
+        types.push(typeof(mix[i]))
+    }
+    return types;
+}
+console.log(dataType());
